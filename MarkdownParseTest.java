e7acdf52e7362dcc6fc73d123cfc0323b7021754
Excalibur021 <haz021@ucsd.edu>
2022-02-25 13:40:44 -0800

    snippets

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -62,4 +62,25 @@ public class MarkdownParseTest {
         List<String> expect = List.of();
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        String contents = Files.readString(Path.of("./snippet1"));
+        List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+        public void testSnippet2() throws IOException {
+            String contents = Files.readString(Path.of("./snippet2"));
+            List<String> expect = List.of("a.com", "a.com(())", "example.com");
+            assertEquals(expect, MarkdownParse.getLinks(contents));
+        }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents = Files.readString(Path.of("./snippet3"));
+        List<String> expect = List.of("https://twitter.com", "https");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
 }
